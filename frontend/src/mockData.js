384b30d5312ac60f74612e395a51130e1af2e1be
TUPPERWAVEX <91781067+TUPPERWAVEX@users.noreply.github.com>
2025-06-11 16:19:57 +1000

    Create mockData.js

---
+++ frontend/src/mockData.js
@@ -0,0 +1,11 @@
+{
+  address: "224 Ida Brisbane Road",
+  coords: [-27.486, 153.018],
+  flood_risk: "medium",
+  train_distance_km: 0.9,
+  crime_rate: "moderate",
+  zoning: "residential_low_density",
+  broadband: "fttp",
+  school_catchment: "top_rated_school",
+  da_activity: "moderate_density"
+}
