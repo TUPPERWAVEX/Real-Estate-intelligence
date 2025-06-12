4f37a14d9c5c8e71c52bb8394271ee96f203b0f3
TUPPERWAVEX <91781067+TUPPERWAVEX@users.noreply.github.com>
2025-06-12 14:42:33 +1000

    Create Home.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+import MapView from "../components/MapView";
+import { dummyProperties } from "../data/dummyProperties";
+
+const Home = () => {
+  return <MapView properties={dummyProperties} />;
+};
+
+export default Home;
