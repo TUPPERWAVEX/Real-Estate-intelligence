af12508846ba40047ad0c7bc7c171239444acfc2
TUPPERWAVEX <91781067+TUPPERWAVEX@users.noreply.github.com>
2025-06-12 14:41:47 +1000

    Create mapView.tsx

---
+++ frontend/src/components/mapView.tsx
@@ -0,0 +1,41 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Property } from '../types';
+import { scoreProperty } from '../utils/scoring';
+
+interface MapViewProps {
+  properties: Property[];
+}
+
+const MapView = ({ properties }: MapViewProps) => {
+  return (
+    <MapContainer center={[-27.47, 153.02]} zoom={12} style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      {properties.map((property) => {
+        const { totalScore, breakdown } = scoreProperty(property);
+        return (
+          <Marker key={property.id} position={[property.lat, property.lng]}>
+            <Popup>
+              <div>
+                <strong>{property.address}</strong><br />
+                🛏 {property.bedrooms} | 🛁 {property.bathrooms}<br />
+                <strong>Score: {totalScore}/100</strong>
+                <ul style={{ paddingLeft: 16 }}>
+                  <li>Flood: {breakdown.floodRisk}</li>
+                  <li>Transport: {breakdown.transportProximity}</li>
+                  <li>Crime: {breakdown.crimeRate}</li>
+                  <li>School: {breakdown.schoolCatchment}</li>
+                  <li>Lifestyle: {breakdown.lifestyle}</li>
+                </ul>
+              </div>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
+  );
+};
+
+export default MapView;
